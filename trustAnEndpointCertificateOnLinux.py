7d890dc433601b268c6aef9784e66967fcf21934
danielElopez <dlopez@osisoft.com>
2019-06-19 16:54:46 -0500

    Initial upload

---
+++ trustAnEndpointCertificateOnLinux.py
@@ -0,0 +1,8 @@
+#  No Warranty or Liability.  The code contained herein is being supplied to Licensee
+#  "AS IS" without any warranty of any kind.  OSIsoft DISCLAIMS ALL EXPRESS AND IMPLIED WARRANTIES,
+#  INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+#  PURPOSE and NONINFRINGEMENT. In no event will OSIsoft be liable to Licensee or to any third party
+#  for damages of any kind arising from Licensee's use of the this code OR OTHERWISE, including
+#  but not limited to direct, indirect, special, incidental and consequential damages, and Licensee
+#  expressly assumes the risk of all such damages.  FURTHER, THIS CODE IS NOT ELIGIBLE FOR
+#  SUPPORT UNDER EITHER OSISOFT'S STANDARD OR ENTERPRISE LEVEL SUPPORT AGREEMENTS
